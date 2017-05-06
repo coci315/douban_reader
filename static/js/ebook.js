ebe0f7a759b3f1ff0a1fdf854e1212c72b854d71
coci315 <coci315@gmail.com>
2017-05-06 23:06:49 +0800

    新增书籍详情页

--- static/js/ebook.js
+++ static/js/ebook.js
@@ -6,16 +6,29 @@ var id = window.location.pathname.match(reg)[1];
 var app = new Vue({
   el: '#app',
   data: {
+    ebookLink: '/reader/ebook/' + id,
     isBundle: false,
     showFull: false,
+    showFullToc: false,
     bundleData: {},
-    bundleWorks: []
+    bundleWorks: [],
+    ebookData: {},
+    reviews: [],
+    showicon: false
+  },
+  computed: {
+    starsCount: function () {
+      return 'stars-' + Math.round(this.ebookData.average_rating);
+    }
   },
   delimiters: ['${', '}'],
   filters: {
     formatPrice: function (price) {
       if (price === 0) return '免费';
       return '￥' + (price / 100).toFixed(2);
+    },
+    formatRating: function (rating) {
+      return (rating * 2).toFixed(1);
     }
   },
   created: async function () {
@@ -28,14 +41,35 @@ var app = new Vue({
     if (this.isBundle) {
       this.bundleData = response1.body;
       document.title = this.bundleData.title + ' | 豆瓣阅读';
+      document.getElementById('g-footer').style.marginBottom = '46px';
       this.bundleWorks = response2.body;
     } else {
-
+      this.ebookData = response1.body;
+      document.title = this.ebookData.title + ' - 作品详情 | 豆瓣阅读';
+      response2.body.forEach(item => item.showFullReview = false);
+      this.reviews = response2.body;
     }
   },
   methods: {
     toggleShowFull: function () {
       this.showFull = !this.showFull;
+    },
+    toggleShowFullToc: function () {
+      if (this.ebookData.table_of_contents.length <= 6) return;
+      this.showFullToc = !this.showFullToc;
+    },
+    toggleShowFullReview: function (item) {
+      item.showFullReview = !item.showFullReview;
+    },
+    loadmore: function () {
+      this.showicon = true;
+      this.$http.get('/ajax/ebook/' + id + '/reviews' + '?start=' + this.reviews.length + '&limit=10').then(response => {
+        this.showicon = false;
+        response.body.forEach(item => item.showFullReview = false);
+        this.reviews = this.reviews.concat(response.body);
+      }, response => {
+        console.log(response);
+      });
     }
   }
 });
